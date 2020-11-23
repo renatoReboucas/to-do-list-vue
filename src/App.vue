034a8aa6090b09b2a4adae2f97a331d83a5263b8
Renato Rebouças <renatoreboucas@MacBook-Pro-de-Renato.local>
2020-11-23 17:14:32 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+
+<template>
+  <div id="app">
+    <h1> To-do list </h1>
+    <input type="text" v-on:keyup.enter="addTodo()" v-model="text">
+    <ul>
+      <li v-for="(item, index) in list" v-bind:key="item" >
+        <TodoItem :item="item" @remove="removeTodo(index)" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/todo-item.vue'
+
+export default {
+  components: {
+    TodoItem
+  },
+  data(){
+    return {
+      text: '',
+      list: [{text: 'Adicionarr mais itens á lista'}]
+    }
+  },
+  methods:{
+    addTodo(){
+      this.list.push({text: this.text})
+      this.text = ''
+      
+    },
+    removeTodo(index){
+      this.list.splice(index, 1)
+    }
+  }
+}
+</script>
+
---
+++ src/components/todo-item.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <span> {{ item.text }} </span>
+    <button @click="$emit('remove')">X</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: Object,
+  },
+};
+</script>
